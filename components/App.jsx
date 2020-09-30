3d3b6bce9697917a1eaa254859b57c2ba48d0d8b
shiv-An <51291155+shiv-An@users.noreply.github.com>
2020-09-30 10:13:24 +0530

    now

--- components/App.jsx
+++ components/App.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import Books from './Books'
+import axios from 'axios';
+import BookSearch from './BookSearch';
+import BooksList from './BooksList';
+
 
 
 class App extends React.Component {
 
+  state = { books:[],page:1};
+
+    onSearchSubmit = async (query) => {
+      const{ books,page} = this.state
+      const url = `http://openlibrary.org/search.json`
+      const response = await axios.get(url,{
+        params:{q:query,page:page}
+      });
+
+      this.setState({books:[...books,...response.data.docs]});
+      console.log(this.state.books)
+  }
+
+  loadMore = ()=>{
+    this.setState(prevState=>({
+      page:prevState.page+1
+    }),this.onSearchSubmit(this.props.query))
+  }
+  
+
   render(){
     return (
       <div>
-      <Books/>
+       <BookSearch onSubmit = {this.onSearchSubmit} />
+       <BooksList books = {this.state.books}/>
+       <button onClick={this.loadMore}>LoadMore</button>
     </div>)
   }
 }
